15c96abd2c31fbbd0b3a2a2b53e2978942abfcba
Sachin V <madarasrini@gmail.com>
2026-03-26 11:17:38 +0530

    force render fix

--- rj-boutique-ecommerce-main/server.ts
+++ rj-boutique-ecommerce-main/server.ts
@@ -9,7 +9,9 @@ import { errorHandler } from './server/middleware/errorHandler';
 
 async function startServer() {
   const app = express();
-  const PORT = Number(process.env.PORT) || 5000;
+  const PORT = process.env.PORT ? Number(process.env.PORT) : 10000;
+
+  // render fix
 
   app.use(cors());
   app.use(express.json());
